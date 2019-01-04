eb3d2d7fe296cb6a6475c71608421d40f81e8101
Eqan Ahmad <33661505+eqanahmad@users.noreply.github.com>
2019-01-04 03:49:58 -0500

    Add files via upload

---
+++ fibsum.m
@@ -0,0 +1,15 @@
+n1 = 1;
+n2 = 2;
+n3 = 3; 
+sum = 2;
+lim = 4e6;
+
+while n3<=4e6
+    n3 = n1 + n2; 
+    n1 = n2;
+    n2 = n3;
+    if mod(n3,2) == 0
+         sum = sum + n3;
+    end
+end
+disp(sum);
