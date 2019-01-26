88b7bdbae6bf624651eb7ea391adaf1714dd8d5b
Eqan Ahmad <33661505+eqanahmad@users.noreply.github.com>
2019-01-25 20:33:36 -0500

    Create projecteuler7.m

---
+++ projecteuler7.m
@@ -0,0 +1,17 @@
+#matlab 
+
+j = 0; 
+x = 1;
+primers = [];
+while x <= 10001
+    if isprime(j) == 1
+        primers(x) = j; 
+        j = j + 1;
+        x = x + 1;
+    else
+        j = j +1;
+    end
+end
+
+disp(j-1);
+
